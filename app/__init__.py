31b88ce8565732a819970519ec8fb20ebb63f156
toomeyDev <mtoomeydev@gmail.com>
2022-11-07 22:22:46 -0500

    Setup SQlite db for testing, setup user model to handle logins
    Create registration form, setup login reqs to view site content

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,17 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from app import routes
+# initialize flask extensions
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+# setup loginManger, associate login handler view
+login = LoginManager(app)
+login.login_view = 'login'
+
+from app import routes, models
--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 # NOTE: temporary development config, REPLACE with separate dev/prod config during production
 
 class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'extremely-secret-key'
+	# SQLAlchemy config
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+		'sqlite:///' + os.path.join(basedir, 'app.db')
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
